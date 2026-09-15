9cd3fc40b4e9d0fb788ac23054066a47edab36d5
FitiavanaMihaja <hei.mickael.122@gmail.com>
2026-09-15 03:30:27 +0300

    first commit

---
+++ src/main/java/school/prog3/hei/prog3/Prog3Application.java
@@ -0,0 +1,13 @@
+package school.prog3.hei.prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3Application.class, args);
+	}
+
+}
---
+++ src/test/java/school/prog3/hei/prog3/Prog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.prog3.hei.prog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
